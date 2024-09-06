8177d410fdb26b854c167a663fcfabac7cb0972b
yuanchangsai77 <yuansaisai33@gmail.com>
2024-09-06 14:43:51 +0800

    运行环境

--- edit.bat
+++ edit.bat
@@ -1,3 +1,8 @@
 @echo off
-venv\Scripts\python.exe edit.py
+:: venv\Scripts\python.exe edit.py
+:: conda activate nlpenv && python edit.py
+
+:: call E:\apps\anaconda\Scripts\activate.bat nlpenv
+:: python edit.py
+E:\apps\anaconda\envs\nlpenv\python.exe edit.py
 pause
